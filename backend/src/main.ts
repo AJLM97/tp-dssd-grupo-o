899d930f397b5a87e7092fd2a5e9da8772a07b8d
AJLM97 <alejandrojavierlemi@gmail.com>
2026-09-19 02:55:18 -0300

    feat: configurar Swagger UI y añadir /api/docs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule, ObserveInstrument } from './app.module';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    instrument: ObserveInstrument,
-  });
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Rentar - Web Services')
+    .setDescription('Documentación de la API REST para el TP de Sistemas Distribuidos (UNLa)')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
